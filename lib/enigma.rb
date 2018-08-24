29d5b11034e4a483f9d2b7f61c8f5682fe0f556f
Noah Flint <noah.flint24@gmail.com>
2018-08-24 14:45:18 -0600

    first_try

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,33 @@
+require 'pry'
+require 'date'
+
 class Enigma
 
+  attr_reader :array
+  attr_reader :my_message
+
+  def intialize (my_message = "this is so secret ..end..")
+    @my_message = my_message
+    @array = ["a", "b", "c", "d", "e",
+          "f", "g", "h", "i", "j",
+          "k", "l", "m", "n", "o",
+           "p", "q", "r", "s", "t",
+         "u", "v", "w", "x", "y", "z",
+         "0", "1", "2", "3", "4", "5",
+         "6", "7", "8", "9", ",", ".", " "]
+       end
+#binding.pry
+
+
+
+  def encrypt
+
+  end
+
+
+
 end
+
+e = Enigma.new
+
+#binding.pry
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,2 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/enigma'
+require 'date'
+
+
+class EnigmaTest < Minitest::Test
+
+  def test_it_exists
+    enigma = Enigma.new
+    assert_instance_of Enigma, enigma
+
+  end
+
+  def test_method_feedback
+    enigma = Enigma.new(my_message)
+    assert_equal "this is so secret ..end..", enigma.my_message
+  end
+
+end
